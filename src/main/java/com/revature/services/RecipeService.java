b5854e040704710c39fc93d7395e2795d0f4c326
Richard Simpson <richard.simpson235@gmail.com>
2022-02-12 17:08:22 -0500

    feat: Add RecipeService interface

---
+++ src/main/java/com/revature/services/RecipeService.java
@@ -0,0 +1,49 @@
+package com.revature.services;
+
+import com.revature.models.Recipe;
+
+import java.util.List;
+
+public interface RecipeService {
+
+    /**
+     *
+     * @param recipe
+     * @return
+     */
+    public Recipe createRecipe(Recipe recipe);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public boolean deleteRecipe(int id);
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public Recipe getRecipeById(int id);
+
+    /**
+     *
+     * @return
+     */
+    public List<Recipe> getAll();
+
+    /**
+     *
+     * @param recipe
+     * @return
+     */
+    public Recipe updateRecipe(Recipe recipe);
+
+    /**
+     *
+     * @param nameSubstring
+     * @return
+     */
+    public List<Recipe> search(String nameSubstring);
+}
