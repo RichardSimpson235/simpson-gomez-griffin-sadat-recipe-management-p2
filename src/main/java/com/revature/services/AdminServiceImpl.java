7729c667c4bc4c2d1711085264717deffb2a7258
Richard Simpson <richard.simpson235@gmail.com>
2022-02-14 12:40:08 -0500

    feat: add dependencies and methods

--- src/main/java/com/revature/services/AdminServiceImpl.java
+++ src/main/java/com/revature/services/AdminServiceImpl.java
@@ -1,4 +1,34 @@
 package com.revature.services;
 
-public class AdminServiceImpl {
+import com.revature.repositories.RecipeRepository;
+import com.revature.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AdminServiceImpl implements AdminService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @Override
+    public boolean banUser(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean unbanUser(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean approveRecipe(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean disapproveRecipe(int id) {
+        return false;
+    }
 }
