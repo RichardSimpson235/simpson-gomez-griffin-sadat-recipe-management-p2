c8ea85c44fff4d942d8d735176c4bf502cee83f4
Richard Simpson <richard.simpson235@gmail.com>
2022-02-14 11:00:20 -0500

    feat: add media model implementation

--- src/main/java/com/revature/models/Media.java
+++ src/main/java/com/revature/models/Media.java
@@ -1,4 +1,24 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="media")
 public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "media_id", updatable = false)
+    private int id;
+
+    // type is the file extension
+    private String type;
+    private String title;
 }
