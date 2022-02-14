f0a26c8eb4924d0bf207e63445460e0027eb31d1
A.Samad Sadat <sadatabdulsamad@yahoo.com>
2022-02-14 14:29:56 -0500

    feat: add Recipe Model

--- src/main/java/com/revature/models/Recipe.java
+++ src/main/java/com/revature/models/Recipe.java
@@ -1,4 +1,34 @@
 package com.revature.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "recipes")
 public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int recipe_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+    private String description;
+    private int cook_time, servings, likes, dislikes;
+    private boolean approved;
+
+
+
+
+
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,4 +1,15 @@
 package com.revature.models;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table
 public class User {
+
+    private int id;
 }
