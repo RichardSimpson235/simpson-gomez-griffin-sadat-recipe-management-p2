0e9c203b9f075d2ea22c81c8903c5427d37ff8c0
Alejandro Gomez <alejandroxgomez@gmail.com>
2022-02-15 15:11:34 -0500

    feat: add id, step, text fields

--- src/main/java/com/revature/models/Instruction.java
+++ src/main/java/com/revature/models/Instruction.java
@@ -1,7 +1,25 @@
 package com.revature.models;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name="instructions")
 public class Instruction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instruction_id", updatable = false)
+    private int id;
+
+    private int step;
+
+    private String text;
+
 }
