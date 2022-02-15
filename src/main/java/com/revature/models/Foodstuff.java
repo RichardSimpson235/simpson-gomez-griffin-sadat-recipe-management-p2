8260c18d622aef99d2c67a0e62c06c473629e815
Richard Simpson <richard.simpson235@gmail.com>
2022-02-15 15:51:59 -0500

    feat: add Foodstuff model

---
+++ src/main/java/com/revature/models/Foodstuff.java
@@ -0,0 +1,22 @@
+package com.revature.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "foodstuff")
+public class Foodstuff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50)
+    private String name;
+}
