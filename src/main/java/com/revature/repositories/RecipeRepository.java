8f228666a28e4a58b1c467af7b49ad888aca8169
A.Samad Sadat <sadatabdulsamad@yahoo.com>
2022-02-15 14:27:27 -0500

    feat: added RecipeRepository documentation -> save(), findAll(), findById(), deleteById().

--- src/main/java/com/revature/repositories/RecipeRepository.java
+++ src/main/java/com/revature/repositories/RecipeRepository.java
@@ -3,6 +3,15 @@ package com.revature.repositories;
 import com.revature.models.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * RecipeRepository extends CrudRepository to perform CRUD Operations on the Database
+ * CRUD Methods
+ * save() -> will create new data and also used to update existing data in the database
+ * findAll() -> retrieves the list of all data in the database
+ * findById() -> retrieves data by parsed ID from the database
+ * deleteById() -> deletes data in the database with the parsed ID
+ *
+ */
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
 
 }
