740f677eb62a7a50878498a6a90fef4c4597b340
Richard Simpson <richard.simpson235@gmail.com>
2022-02-15 11:31:58 -0500

    feat: implement search endpoint

--- src/main/java/com/revature/controllers/RecipeController.java
+++ src/main/java/com/revature/controllers/RecipeController.java
@@ -1,4 +1,24 @@
 package com.revature.controllers;
 
+import com.revature.models.Recipe;
+import com.revature.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/recipes/search")
+    public List<Recipe> searchRecipe(@RequestParam Map<String, String> params) {
+        String searchSubstring = params.get("name");
+        return recipeService.search(searchSubstring);
+    }
 }
