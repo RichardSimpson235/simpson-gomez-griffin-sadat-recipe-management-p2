135ba724e79d1c47a1ecafa7a96bfb37dfff5c86
Richard Simpson <richard.simpson235@gmail.com>
2022-02-15 15:52:11 -0500

    feat: add Ingredient model

--- src/main/java/com/revature/models/Ingredient.java
+++ src/main/java/com/revature/models/Ingredient.java
@@ -1,4 +1,34 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ingredients")
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50)
+    private String amount;
+
+    @ManyToOne
+    @JoinColumn(name="foodstuff_id")
+    private Foodstuff item;
+
+    public String getName() {
+        return item.getName();
+    }
+
+    public void setName(String name) {
+        item.setName(name);
+    }
 }
