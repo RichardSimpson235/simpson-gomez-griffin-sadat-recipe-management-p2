b103cfb5d4193531532d43ba186d4e67f6e992b7
Richard Simpson <richard.simpson235@gmail.com>
2022-02-15 16:09:26 -0500

    feat: add cross origin annotation

--- src/main/java/com/revature/controllers/RecipeController.java
+++ src/main/java/com/revature/controllers/RecipeController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import com.revature.models.Recipe;
 import com.revature.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class RecipeController {
 
