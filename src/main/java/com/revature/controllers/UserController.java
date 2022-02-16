8880ce066d750ec2abf9becbe65a8e2df50c2075
Alejandro Gomez <alejandroxgomez@gmail.com>
2022-02-16 15:57:02 -0500

    feat: create UserController - /register/

---
+++ src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.revature.controllers;
+
+import com.revature.models.User;
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
+    public User addUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
