800ac0b7c0aba44738447b1c3b0070d417c9600a
Alejandro Gomez <alejandroxgomez@gmail.com>
2022-02-16 13:51:00 -0500

    feat: create UserController - /register/

--- src/main/java/com/revature/driver/Application.java
+++ src/main/java/com/revature/driver/Application.java
@@ -2,8 +2,10 @@ package com.revature.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.revature")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,4 +1,7 @@
 package com.revature.models;
 
 public class User {
+
+
+
 }
