a8ce1c75e5c02a544d74ae3fbfa12e166e0f2841
Richard Simpson <richard.simpson235@gmail.com>
2022-02-17 15:48:27 -0500

    test: add FindByUsernameAndPassword test. Passes

--- src/test/java/com/revature/integrations/UserIntegrationTest.java
+++ src/test/java/com/revature/integrations/UserIntegrationTest.java
@@ -1,10 +1,27 @@
 package com.revature.integrations;
 
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = com.revature.driver.Application.class)
 @Transactional
 public class UserIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUsernameAndPassword() {
+        Optional<User> opUser = userRepository.findByUsernameAndPassword("username", "password");
+
+        assertTrue(opUser.isPresent());
+    }
 }
