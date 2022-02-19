5fb44d1a9e3151b901d2b29c1162423303cd1255
Richard Simpson <richard.simpson235@gmail.com>
2022-02-19 16:39:36 -0500

    test: declare all columns

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -5,9 +5,9 @@ INSERT INTO recipes (recipe_id, approved, disapproved, cook_time, description, l
 (default, true, false, '1.5 hrs', 'my recipe is tasty', 0, 0, 'recipe', 4, 1),
 (default, true, false, '2.5 hrs', 'other recipe', 0, 0, 'recipe2', 4, 1);
 
-INSERT INTO instructions VALUES
+INSERT INTO instructions (instruction_id, step, text, recipe_id) VALUES
 (default, 1, 'do this thing', 1),
 (default, 1, 'do this other thing', 2);
 
-INSERT INTO foodstuff VALUES (default, 'apple'), (default, 'coffee');
-INSERT INTO ingredients VALUES (default, 1, 1, 1), (default, 2, 2, 1);
+INSERT INTO foodstuff (foodstuff_id, name) VALUES (default, 'apple'), (default, 'coffee');
+INSERT INTO ingredients (ingredient_id, amount, foodstuff_id, recipe_id) VALUES (default, '1', 1, 1), (default, '2', 2, 1);
