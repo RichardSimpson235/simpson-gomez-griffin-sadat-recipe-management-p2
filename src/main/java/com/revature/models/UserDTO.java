a32d33eaee4867e57c0287e9e1fbb30e1ea303b5
Richard Simpson <richard.simpson235@gmail.com>
2022-02-19 10:34:43 -0500

    feat: add banned field

--- src/main/java/com/revature/models/UserDTO.java
+++ src/main/java/com/revature/models/UserDTO.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class UserDTO {
 
     private int id;
+    private boolean banned;
     private boolean admin;
     private String firstName;
     private String lastName;
