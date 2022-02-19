edd2104d59df19578d41be499d24f163dd9a1a13
Richard Simpson <richard.simpson235@gmail.com>
2022-02-19 14:46:23 -0500

    test: make transactional. all pass

--- src/test/java/com/revature/integrations/UserServiceIntegrationTest.java
+++ src/test/java/com/revature/integrations/UserServiceIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = com.revature.driver.Application.class)
+@Transactional
 public class UserServiceIntegrationTest {
 
     @Autowired
