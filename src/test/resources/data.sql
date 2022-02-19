441a18f5b1be856a2c4d3785c1acb8ac95b8ebce
Richard Simpson <richard.simpson235@gmail.com>
2022-02-19 13:49:18 -0500

    test: make user entity fields explicit in insert

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users VALUES
+INSERT INTO users (user_id, banned, admin, date_of_birth, email, first_name, lasT_name, password, phone, registration_date, username) VALUES
 (default, false, false, 0, 'email@gmail.com', 'first', 'last', 'password', '555-5555', 0, 'username');
 
 INSERT INTO recipes (recipe_id, approved, disapproved, cook_time, description, likes, dislikes, name, servings, user_id) VALUES
